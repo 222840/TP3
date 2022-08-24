08b0bb603d30c207e5028c37d9c9bcefb1f691c7
martin.bisson <mbisson4@hotmail.com>
2022-08-24 15:22:01 -0400

    ajout 3 blog dans menu +foot page

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 from ordinaland import *
 from flask import Flask,request,render_template
 import datetime
-maintenent=datetime.datetime.day
-
+maintenent=datetime.datetime.now()
+annee=maintenent.year
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 
-    return render_template("index.html")
+    return render_template("index.html",annee=annee,titre1=articles[0].titre,texte1=articles[0].texte,
+                           titre2=articles[1].titre,texte2=articles[1].texte,titre3=articles[2].titre,texte3=articles[2].texte)
 
 @app.route("/construire/")
 def construire():
 
-    return render_template("construire.html")
+    return render_template("construire.html",annee=annee)
 
-@app.route("/blog/")
+@app.route("/blog/<int:nb>")
 def blog():
 
-    return render_template("blog.html")
+    return render_template("blog.html",annee=annee)
 
 @app.route("/glossaire/")
 def glossaire():
 
-    return render_template("glossaire.html")
+    return render_template("glossaire.html",annee=annee)
 
 @app.route("/contact/")
 def contact():
 
-    return render_template("contact.html")
+    return render_template("contact.html",annee=annee)
 
 
 if __name__ == "__main__":
